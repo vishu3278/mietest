0eccdf29c4345730f048aedd68672d7019a8fbd7
unknown <vishu3278@gmail.com>
2016-03-11 17:04:04 +0530

    login

--- app.js
+++ app.js
@@ -1,68 +1,58 @@
 var HOSTURL = 'http://localhost/mitalumni/';
 var loggedUserId = 0;
 
-$(document).on('pageinit','#welcome',function(){
 
-	var output = $("#output") ;
+function loginProcess () {
+	var user_email = $("#email").val();
+    if (user_email == null || user_email == "") {
+        alert("Please Enter Email ID");
+    };
+    var password = $("#password").val();
+    if (password == null || password == "") {
+        alert("Please Enter Password");
+    };
+    var loginobj = {
+        "email": user_email,
+        "password": password
+    };
 
-	$("#loginForm").validate({
-			errorPlacement: function(error, element) {
-				error.insertBefore();
-			},
-			
-			submitHandler: function(form){
-				
-				var user_email = $("#email").val();
-			    if (user_email == null || user_email == "") {
-			        alert("Please Enter Email ID");
-			    }
-			    var password = $("#password").val();
-			    if (password == null || password == "") {
-			        alert("Please Enter Password");
-			    }
-			    var loginobj = {
-			        "email": user_email,
-			        "password": password
-			    }
+    var output = $("#output");
+    output.hide();
 
-				$.ajax({
-					url: HOSTURL+'apis/login.php',
-					dataType: 'json',
-					data: JSON.stringify(loginobj),
-					type : 'POST',
-					contentType: 'application/json',		// The content type used when sending data to the server.
-					cache: false,				// To unable request pages to be cached
-					processData:false,			// To send DOMDocument or non processed data file it is set to false
-					success: function (data, status, jqXHR) {
-                		// var response = JSON.stringify(data);
-                		// console.log(data.status);
-						if(data.status==1){
-							// alert(data.data.user.full_name);
-							loggedUserId = data.data.user.user_id;
-							// console.log(data.data.user.user_id);
-							$.mobile.changePage("#profile", {transition: "slideup"});
-						}else{
-							alert("Wrong login details.");
-						}
-					},
-					error: function(){
-						output.html('<p>There was an error loading the data</p>');
-					}
+	$.ajax({
+		url: HOSTURL+'apis/login.php',
+		dataType: 'json',
+		data: JSON.stringify(loginobj),
+		type : 'POST',
+		contentType: 'application/json',		// The content type used when sending data to the server.
+		cache: false,				// To unable request pages to be cached
+		processData:false,			// To send DOMDocument or non processed data file it is set to false
+		success: function (data, status, jqXHR) {
+			// console.log(data);
+			if(data.status==1){
+				// alert(data.data.user.full_name);
+				loggedUserId = data.data.user.user_id;
+				localStorage.loggedUserId = data.data.user.user_id;
+				console.log(data.data.user.user_id);
+				$.mobile.changePage("#profile", {transition: "slideup"});
+			}else{
+				// alert(data.msg);
+				output.html("<p>Something went wrong while processing the ajax request.</p>").show("fast").delay(5000).hide("slow");
+			}
+		},
+		error: function(x,y,z){
+			alert('An error has occurred:\n' + x + '\n' + y + '\n' + z);
+		}
 
 
-				});
-				
-			}	
 	});
-	
-	
-});
+}
 
-$(document).on('pageinit','#profile',function(){
+$(document).on('pagecreate','#profile',function(){
 	var loginobj = {
-	        "user_id": loggedUserId
-	    }
-	console.log(loggedUserId);
+	        "user_id": localStorage.loggedUserId
+	    };
+	console.log(localStorage.loggedUserId + " + " );
 	$.ajax({
 		url: HOSTURL+'apis/user-profile.php',
 		dataType: 'json',
@@ -72,20 +62,29 @@ $(document).on('pageinit','#profile',function(){
 		cache: false,				// To unable request pages to be cached
 		processData:false,			// To send DOMDocument or non processed data file it is set to false
 		success: function (data, status, jqXHR) {
-    		// var response = JSON.stringify(data);
-    		// console.log(data.status);
+			console.log(data);
 			if(data.status==1){
-				// alert(data.data.user.full_name);
-				$("#name").val(data.data.user.full_name);
-				console.log(data.data.user.full_name);
-				
+				if(data.data.user[0].photo == ''){
+					$("#profileImg").attr('src', 'img/default.jpg' );
+				}else{
+					$("#profileImg").attr('src', HOSTURL + data.data.user[0].photo);
+				};
+				$("#userName").html(data.data.user[0].display_name);
+				$("#profile #name").val(data.data.user[0].full_name);
+				$("#profile #email").val(data.data.user[0].email);
+				$("#profile #phone").val(data.data.user[0].mobile);
+				$("#profile #batch").val(data.data.user[0].passing_year);
+				$("#profile #branch").val(data.data.user[0].branch);
+				$("#profile #occup").val(data.data.user[0].current_occupation);
+				$("#profile #location").val(data.data.user[0].business_address);
+				$("#profile #hobby").val(data.data.user[0].current_occupation);
 				// $.mobile.changePage("#profile", {transition: "slideup"});
 			}else{
 				alert(data.msg);
 			}
 		},
-		error: function(){
-			output.html('<p>There was an error loading the data</p>');
+		error: function(x,y,z){
+			alert('An error has occurred:\n' + x + '\n' + y + '\n' + z);
 		}
 
 
