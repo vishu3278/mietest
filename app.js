1920deffbcf352e35787846c066cac52900d9077
unknown <vishu3278@gmail.com>
2016-03-15 17:59:05 +0530

    app

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function loginProcess () {
 
     
     if(loginobj.email != '' && loginobj.password !== ''){
-    	console.log(loginobj);
+    	// console.log(loginobj);
 		$.mobile.loading("show",{
 			textVisible: true,
 			theme:"b"
@@ -34,18 +34,18 @@ function loginProcess () {
 			cache: false,				// To unable request pages to be cached
 			processData:false,			// To send DOMDocument or non processed data file it is set to false
 			success: function (data, status, jqXHR) {
-				// console.log(data);
+				console.log(data);
 				$.mobile.loading("hide");
 				if(data.status==1){
 					// alert(data.data.user.full_name);
 					loggedUserId = data.data.user.user_id;
 					localStorage.loggedUserId = data.data.user.user_id;
+					localStorage.loggedFullName = data.data.user.full_name;
 					// console.log(data.data.user.user_id);
-					
 					$.mobile.changePage("#profile", {transition: "slideup"});
 				}else{
 					alert(data.error);
-					console.log(data.error);
+					// console.log(data.error);
 					// output.html("<p>Something went wrong while processing the ajax request.</p>").show("fast").delay(5000).hide("slow");
 
 				}
@@ -62,7 +62,50 @@ function loginProcess () {
     	alert("Please enter Password");
     };
 	
-}
+};
+
+function regProcess() {
+	var regform = $("#regForm"),
+		name = regform.find("input#name").val(),
+		email = regform.find("input#email").val(),
+		mobile = regform.find("input#mobile").val(),
+		date = regform.find("input#date").val(),
+		branch = regform.find("input#branch").val(),
+		occup = regform.find("input#occup").val(),
+		address = regform.find("input#address").val(),
+		pass1 = regform.find("input#pass1").val(),
+		pass2 = regform.find("input#pass2").val();
+	
+	var regisObj = {"email":email,"password":pass1,"cnf_password":pass2,"full_name":name,"user_mobile":mobile,"passing_year":date,"branch":branch,"current_occupation":occup,"business_address":address};
+		
+	// console.log(regisObj);
+	console.log(JSON.stringify(regisObj));
+
+	$.ajax({
+		url: HOSTURL + 'apis/regtration.php',
+		dataType: 'jsonp',
+		data: JSON.stringify(regisObj),
+		type : 'POST',
+		contentType: 'application/json',
+		cache: false,
+		processData:false,
+		success: function (data, status, jqXHR) {
+			console.log(data);
+			if(data.status==1){
+				alert(data.data.user.full_name);
+				console.log(data.data.user.user_id);
+				
+			}else{
+				alert(data.error);
+			}
+		},
+		error:function () {
+			// alert("Ajax error");
+			console.error("Ajax error. Data captured. Check database.")
+		}
+	});	
+
+};
 
 $(document).on('pagecreate','#profile',function(){
 	var loginobj = {
@@ -96,7 +139,7 @@ $(document).on('pagecreate','#profile',function(){
 				$("#profile #hobby").val(data.data.user[0].current_occupation);
 				// $.mobile.changePage("#profile", {transition: "slideup"});
 			}else{
-				alert(data.msg);
+				alert(data.error);
 			}
 		},
 		error: function(x,y,z){
@@ -125,7 +168,7 @@ $(document).on('pagecreate','#list',function(){
 			// console.log(data);
 			if(data.status==1){
 				// alert(data.data.user.full_name);
-				console.log(data.data.user[0].full_name);
+				// console.log(data.data.user[0].full_name);
 				for(i=0; i<data.data.user.length; i++){
 					var listItem = '<li class="ui-li-has-alt ui-li-has-thumb" data-userid="'+data.data.user[i].user_id+'"><a href="#" class="ui-btn"><img src="'+HOSTURL+data.data.user[i].photo+'" class="ui-li-thumb" width="100" height="100"><h2>'+data.data.user[i].full_name+' <small>(MD)</small></h2><p>'+data.data.user[i].email+'<br>9876543210</p><div class="ui-li-aside"><strong>'+data.data.user[i].passing_year+'</strong><br><small>Batch</small></div></a><a href="#" class="ui-icon-comment ui-nodisc-icon ui-alt-icon ui-btn ui-btn-icon-notext ui-icon-user" title=""></a></li>';
 					userList.append(listItem);
@@ -133,8 +176,8 @@ $(document).on('pagecreate','#list',function(){
 				userList.listview( "refresh" );
 				// $.mobile.changePage("#profile", {transition: "slideup"});
 			}else{
-				// alert(data.msg);
-				output.html("<p>Something went wrong while processing the ajax request.</p>").show("fast").delay(5000).hide("slow");
+				alert(data.error);
+				// output.html("<p>Something went wrong while processing the ajax request.</p>").show("fast").delay(5000).hide("slow");
 			}
 		},
 		error: function(x,y,z){
@@ -176,7 +219,7 @@ $(document).on('pagecreate','#list',function(){
 				// userList.listview( "refresh" );
 				// $.mobile.changePage("#profile", {transition: "slideup"});
 			}else{
-				// alert(data.msg);
+				alert(data.error);
 				output.html("<p>Something went wrong while processing the ajax request.</p>").show("fast").delay(5000).hide("slow");
 			}
 		},
@@ -184,4 +227,46 @@ $(document).on('pagecreate','#list',function(){
 			alert('An error has occurred:\n' + x + '\n' + y + '\n' + z);
 		}
 	});
-})
+});
+
+
+/* -- add event -- */
+function addEvent() {
+	// alert("click");
+	var evtName = $("#addEventForm #eventname").val(),
+		evtDate = $("#addEventForm #eventdate").val(),
+		evTime = $("#addEventForm #eventime").val(),
+		evtAddress = $("#addEventForm #eventaddress").val(),
+		evtImg = $("#addEventForm #eventimg").val(),
+		evtDetail = $("#addEventForm #eventdetail").val();
+		
+	var eventObj = {"title":evtName,"description":evtDetail,"address":evtAddress,"event_date":evtDate,"event_time":evTime,"organizer":localStorage.loggedFullName,"banner":HOSTURL+"img/default.jpg","user_id":localStorage.loggedUserId};
+
+	console.log(JSON.stringify(eventObj));
+
+	$.ajax({
+		url: HOSTURL+'apis/event-add.php',
+		dataType: 'json',
+		data: JSON.stringify(eventObj),
+		type : 'POST',
+		contentType: 'application/json',		// The content type used when sending data to the server.
+		cache: false,				// To unable request pages to be cached
+		// processData:false,			// To send DOMDocument or non processed data file it is set to false
+		success: function (data, status, jqXHR) {
+			console.log(data);
+			if(data.status==1){
+				// alert(data.data.user.full_name);
+				console.log(data.data.user[0].full_name);
+				
+				// $.mobile.changePage("#profile", {transition: "slideup"});
+			}else{
+				alert(data.error);
+				// output.html("<p>Something went wrong while processing the ajax request.</p>").show("fast").delay(5000).hide("slow");
+			}
+		},
+		error: function(){
+			// alert('An error has occurred:\n' + x + '\n' + y + '\n' + z);
+			console.error("Error");
+		}
+	});
+}
